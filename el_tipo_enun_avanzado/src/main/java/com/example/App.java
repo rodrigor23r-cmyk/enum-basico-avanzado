e3c0a95ddf686dab9d0aea9b3f18106d9fdf9e14
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-04-29 19:12:54 +0200

    comienzo de la nueva rama

--- el_tipo_enun_avanzado/src/main/java/com/example/App.java
+++ el_tipo_enun_avanzado/src/main/java/com/example/App.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
 /*
  * el enum tiene un método que se llama values()
  */
@@ -12,29 +7,6 @@ public class App {
 	
     public static void main(String[] args) {
 
-    	// Sentencia FOR mejorado:
-    	
-    	for (Season season : Season.values()) {
-    		System.out.println("nombre: "+ season.name()
-    		+ ", ordinal: " + season.ordinal());
-    	}
-    	
-    	/* Sentencia operaciones de agregado
-    	
-    	 List<Season> season = new ArrayList<Season>();
-    	
-    	new ArrayList<Season>(Arrays.asList(Season.values()));
-    	*/
-    	
-    	 Stream<Season> flujoDeSeason = Arrays.stream(Season.values());
-    	
-    	 flujoDeSeason.forEach(season -> System.out.println("nombre: " + season.name() + ", ordinal: " + season.ordinal()));
-    	  
-    	 // sin declarar la variable flujoDeSeason
-    	 Arrays.stream(Season.values()).forEach(sea -> System.out.println("nombre: " + sea.name() + ", ordinal: " + sea.ordinal()));
-    	 /*
-    	  * el método stream de la clase arrays genera un flujo a partir un array
-    	  * */
     	 
     }
 }
