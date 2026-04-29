853bc3b6d6e925515ffa08ebb22916ec78b8b9e8
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-04-29 14:57:47 +0200

    explicado el recorrer un enum

--- el_tipo_enun_avanzado/src/main/java/com/example/App.java
+++ el_tipo_enun_avanzado/src/main/java/com/example/App.java
@@ -1,4 +1,10 @@
 package com.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 /*
  * el enum tiene un método que se llama values()
  */
@@ -13,7 +19,22 @@ public class App {
     		+ ", ordinal: " + season.ordinal());
     	}
     	
-    	// Sentencia operaciones de agregado:
+    	/* Sentencia operaciones de agregado
     	
+    	 List<Season> season = new ArrayList<Season>();
+    	
+    	new ArrayList<Season>(Arrays.asList(Season.values()));
+    	*/
+    	
+    	 Stream<Season> flujoDeSeason = Arrays.stream(Season.values());
+    	
+    	 flujoDeSeason.forEach(season -> System.out.println("nombre: " + season.name() + ", ordinal: " + season.ordinal()));
+    	  
+    	 // sin declarar la variable flujoDeSeason
+    	 Arrays.stream(Season.values()).forEach(sea -> System.out.println("nombre: " + sea.name() + ", ordinal: " + sea.ordinal()));
+    	 /*
+    	  * el método stream de la clase arrays genera un flujo a partir un array
+    	  * */
+    	 
     }
 }
