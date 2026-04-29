941f12c649501b80f70220934cb575b01be67fbd
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-04-29 14:30:52 +0200

    lo básico del ENUM explicado

--- el_tipo_enun_avanzado/src/main/java/com/example/App.java
+++ el_tipo_enun_avanzado/src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
-
-/**
-
+/*
+ * el enum tiene un método que se llama values()
  */
 public class App {
 	
     public static void main(String[] args) {
 
+    	// Sentencia FOR mejorado:
     	
+    	for (Season season : Season.values()) {
+    		System.out.println("nombre: "+ season.name()
+    		+ ", ordinal: " + season.ordinal());
+    	}
+    	
+    	// Sentencia operaciones de agregado:
     	
     }
 }
